5e9a8365f752f3b04e64e5c2e47141fd38b96846
4edorov <4edorov@gmail.com>
2017-11-08 07:37:42 +0300

    initial table #1

---
+++ pwa/src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout>
+    <q-toolbar slot="header" class="glossy">
+      <q-toolbar-title>
+        Tic Tac Toe Game
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <router-view />
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
