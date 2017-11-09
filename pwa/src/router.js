770220015d7e66d7ff412c059e1461f0f52ce831
4edorov <4edorov@gmail.com>
2017-11-09 07:33:41 +0300

    board desk

--- pwa/src/router.js
+++ pwa/src/router.js
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 function load (component) {
-  // '@' is aliased to src/components
   return () => import(`@/${component}.vue`)
 }
 
@@ -27,7 +26,6 @@ export default new VueRouter({
   routes: [
     { path: '/', component: load('MainPage') },
 
-    // Always leave this last one
     { path: '*', component: load('Error404') } // Not found
   ]
 })
